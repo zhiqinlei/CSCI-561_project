8df6feff706edf4d5e492c6de6cd967d0bfe85ea
Zhiqin Lei <zhiqinlei1998@gmail.com>
2020-09-18 18:14:09 -0700

    Complete HW01

--- HW1/homework3.py
+++ HW1/homework3.py
@@ -2,6 +2,8 @@
 # Zhiqin Lei
 # USCID: 1436737564
 
+# input a 3D maze and use three methods to find the optimal path and output it
+
 import math
 import collections
 import time
@@ -13,27 +15,26 @@ from queue import PriorityQueue
 
 
 # read input file
-start_time = time.time()
-f = open("input6.txt", 'r+')
+start_time = time.time() # record the time cost 
+f = open("input.txt", 'r+')
 search_method = f.readline().rstrip('\n') #first line: search method
 maze = [int(i) for i in f.readline().rstrip('\n').split(' ')] #second line: size of x,y,z 
 start = [int(i) for i in f.readline().rstrip('\n').split(' ')] #third line: entrance grid location
 end = [int(i) for i in f.readline().rstrip('\n').split(' ')] #fourth line: exit grid location
-num = int(f.readline().rstrip('\n')) #num of grids in maze
+num = int(f.readline().rstrip('\n')) #fifthe line: num of grids in maze
 
 grids_list = [] #list of grids and actions eg: [[1, 3, 1, 5], [1, 3, 2, 1, 11]]
-grid = [] #list of grids
-action = [] #list of actions
-for n in range(num):
-    grids_list.append([int(i) for i in f.readline().rstrip('\n').split(' ')])
+
+for n in range(num): #add follwing n lines into grdis_list
+    grids_list.append([int(i) for i in f.readline().rstrip('\n').split(' ')]) 
     
 
 #define an act function
-def act(g, a):
-    #calculate cost
-    if 1 <= a & a <= 6:
+def act(g, a): # input grid and action, output next grid and cost
+
+    if 1 <= a & a <= 6:     # go side, each step cost 10
         c = 10
-    elif 7<= a & a <= 18:
+    elif 7<= a & a <= 18:   # go diagonal, each step cost 14
         c = 14
     if a == 1:
         g[0] += 1
@@ -91,7 +92,7 @@ class Graph:
     # Constructor 
     def __init__(self): 
   
-        # default dictionary to store adjacent 
+        # use default dictionary to store adjacent 
         self.adjacent = defaultdict(list) 
         # use dic to store cost
         self.weights = defaultdict(list)
@@ -100,6 +101,7 @@ class Graph:
     def addEdge(self,u,v): 
         self.adjacent[u].append(v) 
     
+    # function to add weighted cost
     def addCost(self,u,v,c):
         self.adjacent[u].append([v,c])
     
@@ -115,101 +117,96 @@ def backtrace(parent, start, end):
 
 # Function of BFS method
 def BFS(graph, start, end): 
-    parent = {}
-    visited, queue = set(), [start]
+    parent = {}                             # parent records the parent of grid 
+    visited = set()                         # record visited grids
+    queue = [start]                         # use a queue to find optimal path
     while queue:
-        node = queue.pop(0)
-        if node not in visited:
-            visited.add(node)
-            if node == end:
+        grid = queue.pop(0)                 # pop grid from queue
+        if grid not in visited:
+            visited.add(grid)               # if not visited before add it to visited
+            if grid == end:                 # if find the end, return path
                 path = backtrace(parent, start, end)
                 return path
-            for n in graph.adjacent[node]: #check near nodes
-                if n not in visited:
-                    parent[n] = node
-                    queue.append(n)
+            for n in graph.adjacent[grid]:  #check adjacent grids
+                if n not in visited:        # if not visited before
+                    parent[n] = grid        # record the parent
+                    queue.append(n)         # add adjacent to queue
                 
 
 # Function of UCS method
-def UCS(graph, start, end):
+def UCS(graph, start, end): # similar to BFS except weighted edge           
     parent = {}
-    visited, queue = set(), PriorityQueue()
-    queue.put((0, start, [0]))
+    visited = set()
+    queue = PriorityQueue()                 # use priority queue to always pop least cost grid
+    queue.put((0, start, [0]))              # put total cost, start grid and cost each step in to queue
     while queue:
-        cost, node, cpath= queue.get() 
-        if node not in visited:
-            visited.add(node)
-            if node == end:
+        cost, grid, cpath= queue.get()      # use cpath to record every step cost in the path
+        if grid not in visited:
+            visited.add(grid)
+            if grid == end:
                 path = backtrace(parent, start, end)
                 return path, cost, cpath
-            for i in graph.adjacent[node]:
+            for i in graph.adjacent[grid]:
                 if i[0] not in visited:
-                    parent[i[0]] = node
-                    new_path = list(cpath)
-                    new_path.append(i[-1])
+                    parent[i[0]] = grid
+                    new_path = list(cpath)  # use new_path as a temple list of cpath
+                    new_path.append(i[-1])  # update step cost and total cost 
                     total_cost = cost + i[-1]
                     queue.put((total_cost, i[0], new_path))
 
-def heuristic(a, b):
+# Heuristic function for calculating future cost
+def heuristic(a, b): # find the direct distance from a to b, since each step costs 10, times result 10.             
     (x1, y1, z1) = a
     (x2, y2, z2) = b
     return (math.sqrt((x2-x1)**2+(y2-y1)**2+(z2-z1)**2))*10
 
-def A(graph, start, end):
+# Function of A* method
+def A(graph, start, end):   # similar to UCS except heuristic funciton
     queue = PriorityQueue()
     queue.put(start, 0)
     parent = {}
-    cost = {}
-    cost[start] = 0
-
+    cost = {}                              
+    cost[start] = 0                         # use cost to record total cost   
     cpath = {}
-    cpath[start] = 0
+    cpath[start] = 0                        # use cpath to record step cost
     while queue:
-        node = queue.get()
-        if node == end:
+        grid = queue.get()
+        if grid == end:
             path = backtrace(parent, start, end)
             return (path, cost[end], cpath)
-        for i in graph.adjacent[node]:
-            newcost = cost[node] + i[-1]
+        for i in graph.adjacent[grid]:      # i: [(grid), cost]
+            newcost = cost[grid] + i[-1]
             if i[0] not in cost or newcost < cost[i[0]]:
-                cost[i[0]] = newcost
-                total_cost = newcost + heuristic(i[0], end)
-                parent[i[0]] = node
+                cost[i[0]] = newcost        # total cost = past cost + future cost
+                total_cost = newcost + heuristic(i[0], end)    
+                parent[i[0]] = grid
                 cpath[i[0]] = i[-1]
                 queue.put(i[0], total_cost)
-
-        
-
-                
-
-# Function of A* method
-    #def A(self, start, end):
-    
                         
                         
+# Main: run algorithm and output 
 
-# test command
-# BFS commmand
-
+# Run BFS algorithm
 if search_method == 'BFS':
-    g = Graph()
 
-    for node in grids_list: #get the next grid of every grid in list
-        for a in node[3:]:
-            Next, cost = act(node[:3], a)
-            g.addEdge(tuple(node[:3]), tuple(Next))
+    g = Graph()                             # create graph
 
-    path = BFS(g, tuple(start), tuple(end))
+    for grid in grids_list:                 # get every grid from grids list and add the next grid as edge
+        for a in grid[3:]:
+            Next, cost = act(grid[:3], a)   # since BFS does not mind cost, do not add it to graph
+            g.addEdge(tuple(grid[:3]), tuple(Next))
 
-    if path != None:
+    path = BFS(g, tuple(start), tuple(end)) # run BFS
+
+    if path != None:                        # if find the path
         steps = len(path)
-        total = steps -1
+        total = steps -1                    # hard code since total cost always less 1 than total steps
 
         f = open("output.txt", "w")
         f.write(str(total) + "\n")
         f.write(str(steps) + "\n")
         cost = 0
-        for i in path:
+        for i in path:                      # ans example: '1 3 1 1\n' first three represent grid, last represent step cost
             ans = ' '.join(map(str,i))
             f.write(ans + ' ' + str(cost) + "\n")
             cost = 1
@@ -220,23 +217,22 @@ if search_method == 'BFS':
         f.close()
 
     end_time = time.time()
-    print("time= ", end_time-start_time)
+    print("time= ", end_time-start_time)    # output total time cost
 
 
-#UCS test command
+# Run UCS algorithm
 if search_method == 'UCS':
+
     g = Graph()
 
-    for node in grids_list: #get the next grid of every grid in list
-        for a in node[3:]:
-            Next, cost = act(node[:3],a )
-            g.addCost(tuple(node[:3]), tuple(Next), cost)
+    for grid in grids_list:                 # same as BFS except add edge with cost
+        for a in grid[3:]:
+            Next, cost = act(grid[:3],a )
+            g.addCost(tuple(grid[:3]), tuple(Next), cost)
 
     path, total, cpath = UCS(g, tuple(start), tuple(end))
-    end_time = time.time()
-    print("time= ", end_time-start_time)
 
-    if path != None:
+    if path != None:                        # same as BFS except print total steps 
         steps = len(path)
 
         f = open("output.txt", "w")
@@ -254,14 +250,15 @@ if search_method == 'UCS':
     end_time = time.time()
     print("time= ", end_time-start_time)
 
-# A* test command   
-if search_method == 'A*':
+# Run A* algorithm  
+if search_method == 'A*':                   # same to UCS, except run A* algorithm 
+
     g = Graph()
 
-    for node in grids_list: #get the next grid of every grid in list
-        for a in node[3:]:
-            Next, cost = act(node[:3],a )
-            g.addCost(tuple(node[:3]), tuple(Next), cost)
+    for grid in grids_list: 
+        for a in grid[3:]:
+            Next, cost = act(grid[:3],a )
+            g.addCost(tuple(grid[:3]), tuple(Next), cost)
     
     path, total, cpath = A(g, tuple(start), tuple(end))
 
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # CSCI-561-Foundations-of-Artificial-Intelligence
 homework of CSCI-561 class in 2020 fall
+
+You will write a program that will take an input file that describes the maze configuration, the
+initial entrance grid location, the exit grid location, and characteristics of the agent. You should
+find the optimal path from the initial entrance grid location to that exit grid location. A path is
+composed of a sequence of legal moves. Each legal move consists of moving the agent from a
+point to one of its 18 neighbor points, using one of the elementary actions that are available at
+the current location.
+Your agent must search through possible paths of movements and find the optimal path to
+travel from the entrance to the exit, and then output the results.
+To find the solution you will use the following algorithms:
+- Breadth-first search (BFS)
+- Uniform-cost search (UCS)
+- A* search (A*).
